696aab62503ba361c9e4246162a7f96be436ff62
cc14-gg <castellaciro5@gmail.com>
2026-04-01 22:21:09 -0300

    Update Note.hx
    4 lineas :b

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -1,5 +1,7 @@
 package objects;
 
+import flixel.addons.effects.FlxSkewedSprite;
+
 import backend.animation.PsychAnimationController;
 import backend.NoteTypesConfig;
 
@@ -36,8 +38,10 @@ typedef NoteSplashData = {
  * 
  * If you want to make a custom note type, you should search for: "function set_noteType"
 **/
-class Note extends FlxSprite
+class Note extends FlxSkewedSprite
 {
+	public var mesh:modcharting.SustainStrip = null;
+	public var z:Float = 0;
 	public var extraData:Map<String, Dynamic> = new Map<String, Dynamic>();
 
 	public var strumTime:Float = 0;
